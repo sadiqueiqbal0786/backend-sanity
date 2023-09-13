0d689c2dee8f14f9a0ec89a9ad157b5ed8439695
Sadique Iqbal <sadiqueiqbal.si@gmail.com>
2023-09-13 22:05:59 +0530

    all schemas

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,21 @@
-export const schemaTypes = []
+
+import testimonials from "./testimonials"
+import works from './works'
+import brands from './brands'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+export const schemaTypes = [
+testimonials,
+works,
+brands,
+abouts,
+experiences,
+skills,
+workExperience,
+contact
+
+]
